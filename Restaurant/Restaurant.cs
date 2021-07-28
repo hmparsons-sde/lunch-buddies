c9ca46aefe1c0e759e0fa6f145db627950450c01
Holly Parsons <holly-parsons@outlook.com>
2021-07-28 15:56:18 -0500

    Add project files.

---
+++ Restaurant/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lunch_buddies.RestaurantChoice
+{
+    class Restaurant
+    {
+        private List<string> _restaurantNames = new() { "CiCi's Pizza", "El Mexico", "Chili's", "Chick-fil-a", "Camino Real", "O' Charley's", "Nacho's" };
+        public string Name { get; set; }
+
+        public static void RestaurantChoice()
+        {
+            var random = new Random();
+            int num = random.Next(_restaurantNames.Count);
+            Name = _restaurantNames[num];
+        }
+    }
+}
